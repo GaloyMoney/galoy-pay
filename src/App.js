8d83a39496ecc7a3e0674659266109134b31a788
Kartik Shah <krtk.6160@gmail.com>
2021-03-28 01:41:56 +0530

    add more functionality

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
-import Login from './components/Login';
-import { useRoutes } from 'hookrouter';
+import "./App.css";
+import Login from "./components/Login";
+import { useRoutes } from "hookrouter";
+import Dashboard from "./components/Dashboard";
+
+const isAuthenticated = () => {
+  return !!localStorage.getItem("token");
+};
 
 const routes = {
-  '/': () => <Login />,
+  "/": () => <Login />,
+  "/dashboard": () => (isAuthenticated() ? <Dashboard /> : <Login />),
 };
 
 function App() {
-
   let routeResult = useRoutes(routes);
 
   return routeResult || <Login />;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,112 +1,114 @@
-import { gql, useMutation } from '@apollo/client';
+import { gql, useMutation } from "@apollo/client";
+import { navigate } from "hookrouter";
 import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { validatePhone, validateOtp } from "../utils";
 import Header from "./Header";
 import "./Login.css";
 
 const GENERATE_OTP = gql`
-mutation requestPhoneCode($phone: String!) {
-  requestPhoneCode(phone: $phone) {
-    success
+  mutation requestPhoneCode($phone: String!) {
+    requestPhoneCode(phone: $phone) {
+      success
+    }
   }
-}
-`
+`;
 
 const LOGIN = gql`
-mutation login($phone: String!, $otp: Int!) {
-  login(phone: $phone, code: $otp) {
-    token
+  mutation login($phone: String!, $otp: Int!) {
+    login(phone: $phone, code: $otp) {
+      token
+    }
   }
-}
-`
+`;
 
 export default function Login() {
   const [phone, setPhone] = useState("");
-  const [otpGenerated, setOtpGenerated] = useState(false)
-  const [otp, setOtp] = useState("")
+  const [otpGenerated, setOtpGenerated] = useState(false);
+  const [otp, setOtp] = useState("");
 
   function setAuthenticated(token) {
-    console.log({ token })
-  }
-
-  function validatePhone() {
-    return phone.length > 0;
-  }
-
-  function validateOtp() {
-    return otp.length === 6;
+    localStorage.setItem("token", token);
   }
 
   const [generateOTP, { loading: otpGenerating }] = useMutation(GENERATE_OTP, {
     onCompleted({ requestPhoneCode: { success } }) {
-      console.log({ success })
-      setOtpGenerated(true)
-      console.log(otpGenerated)
+      console.log({ success });
+      setOtpGenerated(true);
+      console.log(otpGenerated);
     },
     onError(error) {
-      console.error(error)
-    }
-  })
+      console.error(error);
+    },
+  });
 
   function submitPhone(event) {
     event.preventDefault();
-    console.log({ phone })
-    generateOTP({ variables: { phone } })
+    console.log({ phone });
+    generateOTP({ variables: { phone } });
   }
 
   const [login, { loading: authenticating }] = useMutation(LOGIN, {
     onCompleted({ login: { token } }) {
-      console.log({ token })
-      setAuthenticated(token)
+      console.log({ token });
+      setAuthenticated(token);
+      navigate("/dashboard", true);
     },
     onError(error) {
-      console.error(error.message)
-    }
-  })
+      console.error(error.message);
+    },
+  });
 
   function submitOtp(event) {
     event.preventDefault();
-    console.log({ otp })
-    login({ variables: { phone, otp: parseInt(otp) } })
+    console.log({ otp });
+    login({ variables: { phone, otp: parseInt(otp) } });
   }
 
   return (
     <div>
       <Header />
       <div className="Login">
-        {!otpGenerated && <Form onSubmit={submitPhone}>
-          <Form.Group size="lg">
-            <Form.Control
-              autoFocus
-              type="tel"
-              placeholder="Enter phone number"
-              value={phone}
-              onChange={(e) => setPhone(e.target.value)}
-            />
-          </Form.Group>
-          <Button block size="lg" type="submit" disabled={!validatePhone()}>
-            Login
+        {!otpGenerated && (
+          <Form onSubmit={submitPhone}>
+            <Form.Group size="lg">
+              <Form.Control
+                autoFocus
+                type="tel"
+                placeholder="Enter phone number"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </Form.Group>
+            <Button
+              block
+              size="lg"
+              type="submit"
+              disabled={!validatePhone(phone)}
+            >
+              Login
             </Button>
-        </Form>
-        }
+          </Form>
+        )}
         {otpGenerating && <p>Loading...</p>}
-        {otpGenerated && <Form onSubmit={submitOtp}>
-          <Form.Group size="lg">
-            <Form.Control
-              autoFocus
-              type="number"
-              placeholder="Enter OTP"
-              value={otp}
-              onChange={(e) => setOtp(e.target.value)}
-            />
-          </Form.Group>
-          <Button block size="lg" type="submit" disabled={!validateOtp()}>
-            Submit
-          </Button>
-        </Form>
-        }
+        {otpGenerated && (
+          <Form onSubmit={submitOtp}>
+            <Form.Group size="lg">
+              <Form.Control
+                autoFocus
+                type="number"
+                placeholder="Enter OTP"
+                value={otp}
+                onChange={(e) => setOtp(e.target.value)}
+              />
+            </Form.Group>
+            <Button block size="lg" type="submit" disabled={!validateOtp(otp)}>
+              Submit
+            </Button>
+          </Form>
+        )}
       </div>
-    </div >
+    </div>
   );
 }
