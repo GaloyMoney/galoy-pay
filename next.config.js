f0aebf283f692e0847189b95cf91bf8d275f2c63
Theophilus Isah <theophilus@Theophiluss-MacBook-Pro.local>
2022-08-12 16:46:47 +0100

    feat: add service worker

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-module.exports = {
+const withPWA = require("next-pwa")
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+    register: true,
+  },
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
@@ -8,4 +15,4 @@ module.exports = {
       { source: "/.well-known/lnurlp/:username", destination: "/api/lnurlp/:username" },
     ]
   },
-}
+})
