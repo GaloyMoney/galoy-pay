db72ef1965f741348eb463f4ca28c50f6d73843b
Nicolas Burtey <nb@galoy.io>
2023-08-21 19:49:11 +0100

    chore: remove is not needed in the initial call

--- app/lnurlp/[username]/route.ts
+++ app/lnurlp/[username]/route.ts
@@ -9,7 +9,6 @@ import {
   HttpLink,
   InMemoryCache,
 } from "@apollo/client"
-import Redis from "ioredis"
 
 import { GRAPHQL_URI_INTERNAL, NOSTR_PUBKEY } from "../../../lib/config"
 import {
@@ -72,34 +71,6 @@ gql`
 `
 
 const nostrEnabled = !!NOSTR_PUBKEY
-
-let redis: Redis | null = null
-
-if (nostrEnabled) {
-  const connectionObj = {
-    sentinelPassword: process.env.REDIS_PASSWORD,
-    sentinels: [
-      {
-        host: `${process.env.REDIS_0_DNS}`,
-        port: 26379,
-      },
-      {
-        host: `${process.env.REDIS_1_DNS}`,
-        port: 26379,
-      },
-      {
-        host: `${process.env.REDIS_2_DNS}`,
-        port: 26379,
-      },
-    ],
-    name: process.env.REDIS_MASTER_NAME ?? "mymaster",
-    password: process.env.REDIS_PASSWORD,
-  }
-
-  redis = new Redis(connectionObj)
-
-  redis.on("error", (err) => console.log({ err }, "Redis error"))
-}
 
 export async function GET(
   request: Request,
