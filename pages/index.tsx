fbe8f881d7737bb19071e75d2ddc11dab74b9ca1
Sam Peters <unclesamtoshi@protonmail.com>
2023-11-28 15:44:34 -0700

    chore: known good

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import Card from "react-bootstrap/Card"
 import Col from "react-bootstrap/Col"
 import Container from "react-bootstrap/Container"
-import Jumbotron from "react-bootstrap/Jumbotron"
 import ListGroup from "react-bootstrap/ListGroup"
 import Row from "react-bootstrap/Row"
 import { gql, useQuery } from "@apollo/client"
@@ -51,80 +50,78 @@ function Home() {
         <Col>
           <h2>Connect to the Blink Node</h2>
           <br />
-          <Jumbotron>
-            <Container>
-              <Row>
-                <Col>
-                  <Card>
-                    <Card.Body>
-                      <ListGroup variant="flush">
-                        <ListGroup.Item>
-                          <label>Node Public Key: </label>{" "}
-                          <p style={{ fontSize: "small", overflowWrap: "break-word" }}>
-                            {error
-                              ? "Unavailable"
-                              : loading
-                              ? "Loading..."
-                              : data.globals.nodesIds[0]}
-                          </p>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                          {error ? (
-                            "Unavailable"
-                          ) : loading ? (
-                            "Loading..."
-                          ) : (
-                            <a href={nodeUrl + `${data.globals.nodesIds[0]}`}>
-                              Connect the Blink node
-                            </a>
-                          )}
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                          <form
-                            className="username-form"
-                            autoComplete="off"
-                            onSubmit={(event: React.FormEvent<HTMLFormElement>) =>
-                              handleSubmit(event)
+          <Container>
+            <Row>
+              <Col>
+                <Card>
+                  <Card.Body>
+                    <ListGroup variant="flush">
+                      <ListGroup.Item>
+                        <label>Node Public Key: </label>{" "}
+                        <p style={{ fontSize: "small", overflowWrap: "break-word" }}>
+                          {error
+                            ? "Unavailable"
+                            : loading
+                            ? "Loading..."
+                            : data.globals.nodesIds[0]}
+                        </p>
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        {error ? (
+                          "Unavailable"
+                        ) : loading ? (
+                          "Loading..."
+                        ) : (
+                          <a href={nodeUrl + `${data.globals.nodesIds[0]}`}>
+                            Connect the Blink node
+                          </a>
+                        )}
+                      </ListGroup.Item>
+                      <ListGroup.Item>
+                        <form
+                          className="username-form"
+                          autoComplete="off"
+                          onSubmit={(event: React.FormEvent<HTMLFormElement>) =>
+                            handleSubmit(event)
+                          }
+                        >
+                          <label htmlFor="username">
+                            To use the <strong>POS</strong> app, enter your blink (BBW)
+                            username
+                          </label>
+                          <input
+                            type="text"
+                            name="username"
+                            value={username}
+                            onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                              setUsername(event.target.value)
                             }
-                          >
-                            <label htmlFor="username">
-                              To use the <strong>POS</strong> app, enter your blink (BBW)
-                              username
-                            </label>
-                            <input
-                              type="text"
-                              name="username"
-                              value={username}
-                              onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
-                                setUsername(event.target.value)
+                            placeholder="username"
+                            required
+                          />
+                          <label htmlFor="display" style={{ alignSelf: "flex-start" }}>
+                            Enter your currency
+                          </label>
+                          <CurrencyDropdown
+                            name="display"
+                            style={{ height: "42px", width: "100%" }}
+                            onSelectedDisplayCurrencyChange={(newDisplayCurrency) => {
+                              if (newDisplayCurrency) {
+                                localStorage.setItem("display", newDisplayCurrency)
+                                setSelectedDisplayCurrency(newDisplayCurrency)
                               }
-                              placeholder="username"
-                              required
-                            />
-                            <label htmlFor="display" style={{ alignSelf: "flex-start" }}>
-                              Enter your currency
-                            </label>
-                            <CurrencyDropdown
-                              name="display"
-                              style={{ height: "42px", width: "100%" }}
-                              onSelectedDisplayCurrencyChange={(newDisplayCurrency) => {
-                                if (newDisplayCurrency) {
-                                  localStorage.setItem("display", newDisplayCurrency)
-                                  setSelectedDisplayCurrency(newDisplayCurrency)
-                                }
-                              }}
-                            />
-                            <button>Submit</button>
-                          </form>
-                        </ListGroup.Item>
-                      </ListGroup>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
-              <hr />
-            </Container>
-          </Jumbotron>
+                            }}
+                          />
+                          <button>Submit</button>
+                        </form>
+                      </ListGroup.Item>
+                    </ListGroup>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+            <hr />
+          </Container>
         </Col>
       </Row>
     </Container>
